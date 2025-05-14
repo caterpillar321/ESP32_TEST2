c316d60003f1617fe2c2442b4a2e376d0bd6a399
caterpillar321 <kbktw0705@gmail.com>
2025-05-14 14:20:12 +0900

    Update esp32.ino

--- esp32.ino
+++ esp32.ino
@@ -1,17 +1,8 @@
 #include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
-
-// OLED 설정
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
-#define OLED_RESET    -1
-
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 // BLE UUID
 #define SERVICE_UUID        "0000feed-0000-1000-8000-00805f9b34fb"
@@ -48,13 +39,6 @@ class WriteCallback : public BLECharacteristicCallbacks {
         Serial.printf("0x%02X ", (uint8_t)value[i]);
       }
       Serial.println();
-
-      display.clearDisplay();
-      display.setCursor(0, 0);
-      display.setTextSize(1);
-      display.setTextColor(SSD1306_WHITE);
-      display.print(value);
-      display.display();
     }
   }
 };
@@ -64,18 +48,6 @@ unsigned long lastNotify = 0;
 void setup() {
   Serial.begin(115200);
   delay(1000);
-
-  // OLED 초기화
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    Serial.println("SSD1306 allocation failed");
-    while (true);
-  }
-  display.clearDisplay();
-  display.setTextSize(1);
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0, 0);
-  display.println("BLE Peripheral");
-  display.display();
 
   // BLE 초기화
   BLEDevice::init("ESP32_OLED_BLE");
